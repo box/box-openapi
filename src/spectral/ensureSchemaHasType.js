25ebb1c7c347ee47afb1c76cdfc655eac636099b
Cristiano Betta <cbetta@box.com>
2020-03-06 14:12:35 -0800

    Adjustments to OpenAPI spec that allow for auto-generation of SDK… (#77)
    * Make OAS3 work with OpenAPI Generator
    
    * Clarify binary content. Update file upload

---
+++ src/spectral/ensureSchemaHasType.js
@@ -0,0 +1,7 @@
+module.exports = (schema, _, paths) => {
+  if (!schema.type && !schema['$ref']) {
+    return [{
+      message: `${paths.target ? paths.target.join('.') : 'type or $ref'} is not truthy`,
+    }]
+  }
+}
