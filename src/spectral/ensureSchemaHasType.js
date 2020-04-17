6b6f74b60f933e22b0bc3a93efbe6bc203c535c9
Cristiano Betta <cbetta@box.com>
2020-04-17 13:34:52 +0100

    Fix: return correct type of shared links API

--- src/spectral/ensureSchemaHasType.js
+++ src/spectral/ensureSchemaHasType.js
@@ -1,4 +1,5 @@
 module.exports = (schema, _, paths) => {
+  if (schema.oneOf) { return }
   if (!schema.type && !schema['$ref']) {
     return [{
       message: `${paths.target ? paths.target.join('.') : 'type or $ref'} is not truthy`,
